9b09d0309838c9eae515b00579b792e99760613e
vivbigelw <vivbigel@indiana.edu>
2019-12-10 00:16:36 -0500

    Added docs

--- squadron.py
+++ squadron.py
@@ -6,7 +6,7 @@ try:
 	import pygame
 	from pygame.locals import *
 	import math
-	from random import random
+#	from random import random
 #	from scipy import signal	
 #	import numpy
 #	from lib.perlin2d import generate_perlin_noise_2d
@@ -21,6 +21,8 @@ except ImportError as err:
 FRAME_RATE = 30
 
 ###########################################################################################################
+#load a png file from the assets directory
+#From the pygame example docs
 def load_png(name):
 	#load a .png and return a pygame.image
 	fullname = os.path.join('assets', name) 
@@ -39,37 +41,52 @@ def load_png(name):
 		
 	
 ############################################################################################################
+#The boom object is created from explosions
+#Updates the terrain bitmap and effects the physics of PhysicsObjects
+#A better implementation might be as an event that gets checked in the game loop 
+
 class Boom():
 	def __init__(self,terrain,sprites,worldX,worldY,radius):
+
+		#the terrain of the game
 		self.terrain = terrain
+
+		#List of all sprites(physics objects) in the game
 		self.sprites = sprites
+
+		#position of the boom
 		self.worldX = worldX
 		self.worldY = worldY
-		self.radius = radius
-		self.circle_bresenham(self.worldX,self.worldY,self.radius)
-		self.shockwave()
 
+		self.radius = radius
+
+		#Bresenham function for creating circles in the bitmap
+		self.circle_bresenham(self.worldX,self.worldY,self.radius)
+		
+		#Function to check and update the physics of objects in the radius of the boom
+		self.shockwave()
+	
+	#Change the color of the pixels in the ny row from start x to end x to black.
+	#Changes all pixels even if they are already black
 	def drawline(self,sx,ex,ny):
 		for i in range(sx,ex):
 			if (ny >= 0 and ny < self.terrain.y and i >= 0 and i < self.terrain.x):
 				self.terrain.bitmap[i,ny] = pygame.Color(0,0,0)			
 
+	#Create a circle with center xc,yc and radius r. 
+	#Use the drawline function to draw each line of the circle
 	def circle_bresenham(self,xc,yc,r):
 		x = 0
 		y = r
-		p = 3-2*r
+		p = 3-(2*r)
 		if not r:
 			return
 		
 		while(y >= x):
 			self.drawline(xc-x,xc+x,yc-y)
-		#	print("line 1")
 			self.drawline(xc-y,xc+y,yc-x)
-		#	print("line 2")
 			self.drawline(xc-x,xc+x,yc+y)
-		#	print("line 3")
 			self.drawline(xc-y,xc+y,yc+x)
-		#	print("line 4")
 			if(p <0):
 				x+=1
 				p+= 4 * x + 6
@@ -77,12 +94,15 @@ class Boom():
 				y-=1
 				x+=1
 				p+=4 * (x - y) + 10
-				 
+	#Check to see how far each sprite is from the boom
+	#If within the radiuis update the velocity vectors 				 
 	def shockwave(self):
 		for sprite in self.sprites:
 			dx = sprite.px - self.worldX
 			dy = sprite.py - self.worldY
 			dist = math.sqrt(dx*dx + dy*dy)
+
+			#No divide by zero
 			if (dist < 0.0001): dist = 0.0001
 
 		if dist < self.radius:
@@ -90,79 +110,96 @@ class Boom():
 			sprite.vy = (dy / dist) * self.radius
 			sprite.stable = False
 					
-				
+#Base Object for objects that move				
 class PhysicsObject():
 	def __init__(self,x=0.0,y=0.0):
+		#position vectors
 		self.px = x
 		self.py = y
+		#velocity vectors
 		self.vx = 0.0
 		self.vy = 0.0
+		#acceleration vectors
 		self.ax = 0.0
 		self.ay = 0.0
-
+		
+		#radius of the bounding circle
 		self.radius =	4.0
 		self.stable = False
 		self.friction = 0.3
 		
 		
-
+#Main character class for the game
+#Extends pygame.Sprite and PhysicsObject
 class Soldier(pygame.sprite.Sprite,PhysicsObject):
 		def __init__(self,name,image,terrain,x=0.0,y=0.0):
 				pygame.sprite.Sprite.__init__(self)
 				PhysicsObject.__init__(self,x,y)
 				self.image, self.rect = load_png(image)
-				self.rect = self.image.get_rect()
+		#		self.rect = self.image.get_rect()
 				self.terrain = terrain
 				self.rect.centerx = self.px 
 				self.rect.centery = self.py 
 
 		
 		def update(self):
-				self.ay += 0.1 
+				#Update physics vectors 
+				self.ay += 0.1 #gravity 
 				self.vx += self.ax 
 				self.vy	+= self.ay 
+				#Calculate the potential position, then check for collision
 				potentialX = self.px + self.vx   
 				potentialY = self.py + self.vy  
 				self.ax = 0.0
 				self.ay = 0.0
 				self.stable = False
 
+				#Find the angle of motion
 				angle = math.atan2(self.vy, self.vx) 
 				responseX = 0
 				responseY = 0
 				collision = False
+				#iterate over points on bounding circle to check for collisions
 				for r in numpy.arange((angle - math.pi/2.0), (angle + math.pi/2.0), (math.pi/8.0)):
 					testPosX = self.radius * math.cos(r) +	potentialX
 					testPosY = self.radius * math.sin(r) + potentialY
 					int_x = int(testPosX)
 					int_y = int(testPosY)
+
+					#Constrain to screen
 					if int_x >= self.terrain.x : int_x = self.terrain.x - 1
 					if int_x < 0 : int_x = 0
 					if int_y >= self.terrain.y : int_y = self.terrain.y - 1
 					if int_y < 0 : int_y = 0	
+					#Check for collision using the color of the bitmap at the test positions
 					if (self.terrain.surface.get_at((int_x,int_y))[0] == 255):
 						responseX += potentialX - testPosX
 						responseY += potentialY - testPosY
 						collision = True
-				magVelocity = math.sqrt(self.vx * self.vx + self.vy * self.vy)
-				magResponse = math.sqrt(responseX * responseX + responseY * responseY)
+					magVelocity = math.sqrt(self.vx * self.vx + self.vy * self.vy)
+					magResponse = math.sqrt(responseX * responseX + responseY * responseY)
 
 				if collision:
 					self.stable = True
+					#Update motion from collision
 					dot = self.vx * (responseX / magResponse) + self.vy * (responseY/magResponse)
 					self.vx = self.friction * (-2.0 * dot * (responseX / magResponse) + self.vx)
 					self.vy = self.friction * (-2.0 * dot * (responseY / magResponse) + self.vy) 
 				else:
-	
 					self.px = potentialX
 					self.py = potentialY
+				#Update the rect of the object
 				self.rect.centerx = self.px
 				self.rect.centery = self.py
+
+				#stop the motion after it gets too low
 				if (magVelocity < 0.1):
 					self.stable = True
 
 				pygame.event.pump()
 		
+"""
+#Debris object that is generate from terrain destruction
 
 class Debris(PhysicsObject):
 	def __init__(self, terrain, x=0.0, y=0.0):
@@ -222,7 +259,7 @@ class Debris(PhysicsObject):
 			self.stable = True
 
 		pygame.event.pump()
-		
+"""		
 ############################################################################################################	 
 def main():
 	#Initialize Screen
@@ -235,10 +272,15 @@ def main():
 		background = background.convert()
 		background.fill((0,0,0))
 		screen.blit(background, (0,0))
+
+		#Initialize Terrain
 		terrain = Terrain(screen)		 
 		screen.blit(terrain.surface, (0,0))
-		#Initialize player
+
+		#Initialize sprite array(Physics Objects array) 
 		sprites = []
+		
+		#Initialize sprite group for drawing soldiers
 		allSprites = pygame.sprite.Group()
 	
 		#Initialize clock
@@ -251,9 +293,11 @@ def main():
 						if event.type == QUIT:
 								keepGoing = False
 						elif event.type == pygame.KEYDOWN:
+							#B button creates a boom object at the mouse position
 							if event.key == K_b:
 								x,y =pygame.mouse.get_pos()
 								boom = Boom(terrain,sprites,x,y,10) 
+						#Mouse Click creates a new soldier at mouse position
 						elif event.type == MOUSEBUTTONDOWN:
 							x,y = pygame.mouse.get_pos()
 							player = Soldier("New", "small_square.png",terrain,x,y)
@@ -261,6 +305,8 @@ def main():
 							sprites.append(player)
 				allSprites.clear(screen, background)
 				allSprites.update()
+
+				#Update for destructions
 				terrain.update()
 				screen.blit(terrain.surface,(0,0))
 				allSprites.draw(screen)
--- terrain.py
+++ terrain.py
@@ -16,7 +16,7 @@ except ImportError as err:
 #FRAME_RATE = 30
 
 #####################################################################################
-
+# convolution kernels
 smallBlur = numpy.ones((7,7), dtype="float") * (1.0/(7*7))			
 largeBlur = numpy.ones((21,21), dtype="float") * (1.0/(21*21))
 sharpen = numpy.array((
@@ -25,16 +25,17 @@ sharpen = numpy.array((
 	[0,-1,0]), dtype ="int")
 
 #######################################################################################
-		
+#Terrain Class that randomly generates a bitmap for drawing the terrain		
 class Terrain(pygame.sprite.Sprite):
 		def __init__(self,screen):
-				pygame.sprite.Sprite.__init__(self)
+			#	pygame.sprite.Sprite.__init__(self)
 				self.screen = screen
 				self.x,self.y = screen.get_size()
 				self.generate_noisemap()
 				self.bitmap = self.generate_bitmap(self.noisemap)
 				self.surface = pygame.PixelArray.make_surface(self.bitmap)
-				
+
+		#generate and modify the noisemap of the terrain 		
 		def generate_noisemap(self):
 			self.noisemap = generate_perlin_noise_2d((self.x,self.y),(20,20))
 			self.noisemap = self.noisemap * 255
@@ -42,6 +43,7 @@ class Terrain(pygame.sprite.Sprite):
 			self.add_light(self.noisemap)
 			self.perform_convolutions()
 
+		#perform specific convolutions on the noisemap
 		def perform_convolutions(self):
 			local_largeBlur = numpy.ones((21,21), dtype="float") * (1.0/(21*21))
 			local_sharpen = numpy.array((
@@ -53,7 +55,8 @@ class Terrain(pygame.sprite.Sprite):
 			self.noisemap = signal.convolve2d(self.noisemap, local_largeBlur, 'same')
 			self.noisemap = signal.convolve2d(self.noisemap, local_largeBlur, 'same')
 			self.noisemap = signal.convolve2d(self.noisemap, local_sharpen, 'same')
-
+		
+		#adjust the noisemap so certain sections will generate black pixels in the bitmap
 		def add_darkness(self,noisemap):
 			for row in range(self.x):
 				for col in range(200):						
@@ -65,20 +68,19 @@ class Terrain(pygame.sprite.Sprite):
 				for col in range(self.y-1, 200, -1):
 					noisemap[row,col] = noisemap[row,col] + 200 
 			
-
+		#adjust the noisemap so certain sections will generate white pixels in the bitmap
 		def add_light(self, noisemap):
 			for row in range(self.x):
 				for col in range(self.y-1, 340, -1):
 					value = noisemap[row,col]
 					noisemap[row,col] = value - 100 
 
+		#use the noisemap to generate the black and white bitmap for drawing the terrain
 		def generate_bitmap(self, noisemap):
 				surface = pygame.Surface((self.x,self.y))
 				bitmap = pygame.PixelArray(surface)
 				for row in range(self.x-1,0,-1):
 						for col in range(self.y-1, 0, -1):
-							#	if self.image.get_at((row,col))[0] == 237:
-							#			bitmap[row,col] = pygame.Color(255,255,255)
 								if noisemap[row,col] < 1:
 									bitmap[row,col] = pygame.Color(255,255,255)
 								
@@ -86,54 +88,5 @@ class Terrain(pygame.sprite.Sprite):
 
 		def update(self):
 			self.surface = pygame.PixelArray.make_surface(self.bitmap)
-"""
-		def flood_fill(self,stack,bitmap,surface):
-				while len(stack) > 0:
-						array = pygame.surfarray.array2d(surface)
-						pt = stack.pop()
-						x = pt[0]
-						y = pt[1]
-						if 
-							bitmap[x,y] = pygame.Color(255,255,255)
-							stack.append((x+1,y))
-							stack.append((x-1,y))
-							stack.append((x, y+1))
-							stack.append((x,y-1))
-						else:
-							bitmap[x,y] = pygame.Color(0,0,0)
-							continue
-	"""						
-				
-				
 
-#####################################################################################
-"""def main():
-		pygame.init()
-		screen = pygame.display.set_mode((640,360))
-		pygame.display.set_caption("Terrain_Testing")
-
-		background = pygame.Surface(screen.get_size())
-		background = background.convert()
-		background.fill((255,255,255))
-		screen.blit(background, (0,0))
-		terrain = Terrain(screen)
-		
-		screen.blit(terrain.surface, (0,0))
-		pygame.display.flip()
-	
-		allSprites = pygame.sprite.Group()
-	
-		keepGoing = True
-		while(keepGoing):
-				for event in pygame.event.get():
-						if event.type == QUIT:
-								keepGoing = False
-				allSprites.clear(screen,background)
-				allSprites.draw(screen)
-				pygame.display.flip()	
-
-
-if __name__=='__main__':
-	main()	
-"""	
-	
+#############################################################################################################
